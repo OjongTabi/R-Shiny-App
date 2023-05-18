aff88e60863e3f51a6b7cf6ec1c6b523db814d90
OjongTabi <123762693+OjongTabi@users.noreply.github.com>
2023-05-18 12:43:26 -0400

    Update Final_BF591_App_V2.R

--- Final_BF591_App_V2.R
+++ Final_BF591_App_V2.R
@@ -1,36 +1,52 @@
-library(shiny)
-library(shinydashboard)
-library(DT)
-library(ggplot2)
-library(dplyr)
-library(colourpicker)
-library(pheatmap)
-library(gridExtra)
-library(tidyr)
-library(forcats)
-library(tibble)
-options(shiny.maxRequestSize = 100 * 1024^2) # 100 MB
+# Importing required libraries/packages
+library(shiny)  # for building interactive web apps
+library(shinydashboard)  # for creating dashboard layouts
+library(DT)  # for interactive tables
+library(ggplot2)  # for creating plots
+library(dplyr)  # for data manipulation
+library(colourpicker)  # for interactive color picking
+library(pheatmap)  # for creating heatmap visualizations
+library(gridExtra)  # for arranging multiple grid-based plots on a page
+library(tidyr)  # for data tidying
+library(forcats)  # for working with categorical variables
+library(tibble)  # for creating and manipulating tibble data frames
+# Increasing the size limit for file uploads to 100 MB
+options(shiny.maxRequestSize = 100 * 1024^2)
 # App 1 - Sample Information Exploration
+# App 1 UI function
 app1_ui <- function(id) {
+  # The NS function generates a namespace function for input and output
   ns <- NS(id)
+  
+  # fluidPage creates a page with automatically adjusting layout
   fluidPage(
-    titlePanel("Sample Information Exploration"),
+    titlePanel("Sample Information Exploration"),  # title of the page
+    
+    # sidebarLayout divides the UI into a sidebar and a main panel
     sidebarLayout(
+      # sidebarPanel contains the input controls
       sidebarPanel(
+        # fileInput creates a file upload control
         fileInput(ns("data_file"), "Upload CSV File",
-                  multiple = FALSE,
-                  accept = c("text/csv",
+                  multiple = FALSE,  # only one file can be uploaded
+                  accept = c("text/csv",  # the types of files that can be uploaded
                              "text/comma-separated-values,text/plain",
                              ".csv")),
+        # uiOutput renders a reactive UI element
         uiOutput(ns("columnSelect")),
+        # actionButton creates a clickable button
         actionButton(ns("plotButton"), "Plot")
       ),
+      
+      # mainPanel contains the output elements
       mainPanel(
+        # tabsetPanel creates a tabbed panel to switch between outputs
         tabsetPanel(
           id = ns("tabs"),
-          tabPanel("Summary", tableOutput(ns("summaryTable"))),
-          tabPanel("Data", DTOutput(ns("dataTable"))),
-          tabPanel("Plots", plotOutput(ns("histogram")))
+          # tabPanel creates an individual tab
+          tabPanel("Summary", tableOutput(ns("summaryTable"))),  # tableOutput renders a reactive table
+          tabPanel("Data", DTOutput(ns("dataTable"))),  # DTOutput renders a DataTable element
+          tabPanel("Plots", plotOutput(ns("histogram")))  # plotOutput renders a reactive plot
         )
       )
     )
@@ -130,56 +146,87 @@ app4_ui <- function(id) {
 
 
 # Combine the UIs into a single Shiny app
+# The navbarPage function creates a page with a top level navigation bar
 ui <- navbarPage("Bioinformatics Processes App",
+                 # tabPanel creates an individual tab
                  tabPanel("Sample Info", app1_ui("app1")),
                  tabPanel("Counts Matrix", app2_ui("app2")),
                  tabPanel("DESeq2 Results", app3_ui("app3")),
                  tabPanel("Gene Expressions", app4_ui("app4")),
+                 # footer adds a footnote to the page
                  footer = "This app allows users to explore and visualize different bioinformatics processes, including sample information exploration, counts matrix exploration, DESeq2 results visualization, and individual gene expressions visualization.")
-
-
-
+                 
+# Server function for App 1
 app1_server <- function(id) {
+  # Using moduleServer to isolate input and output
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
+    # Reactive expression to read the uploaded CSV file
     data <- reactive({
+      # Ensuring file is uploaded before attempting to read
       req(input$data_file)
+      
+      # Reading the CSV file
       df <- read.csv(input$data_file$datapath, stringsAsFactors = FALSE, header = TRUE)
+      
+      # Renaming the first column as "gene"
       colnames(df)[1] <- "gene"
+      
+      # Return the data frame
       df
     })
     
+    # Render the summary table
     output$summaryTable <- renderTable({
+      # Accessing the reactive 'data'
       df <- data()
+      
+      # Creating the summary data frame
       summary_df <- data.frame(Column = colnames(df),
                                Type = sapply(df, class),
                                Mean_SD = sapply(df, function(x) if (is.numeric(x)) paste0(round(mean(x), 2), " (+/- ", round(sd(x), 2), ")") else "NA"),
                                Distinct_Values = sapply(df, function(x) if (is.factor(x)) toString(unique(x)) else "NA"))
+      
+      # Return the summary data frame
       summary_df
     }, rownames = FALSE)
     
+    # Render the data table
     output$dataTable <- renderDT({
+      # Create a datatable from the reactive 'data'
       datatable(data())
     })
     
+    # Reactive expression for numeric columns
     columns <- reactive({
       colnames(data()[, sapply(data(), is.numeric)])
     })
     
+    # UI output for column select input
     output$columnSelect <- renderUI({
       selectInput(ns("column"), "Select a Column", choices = columns())
     })
     
+    # Render the histogram plot
     output$histogram <- renderPlot({
+      # Ensure the column input is selected
       req(input$column)
+      
+      # Accessing the reactive 'data'
       df <- data()
+      
+      # Selected column for the plot
       plot_column <- input$column
+      
+      # Check if a column is selected
       if (!is.null(plot_column) && plot_column != "") {
+        # Plotting with selected column
         ggplot(df, aes(x = !!sym(plot_column))) +
           geom_histogram(bins = 30, fill = "steelblue", color = "black") +
           labs(x = plot_column, y = "Count", title = "Histogram")
       } else {
+        # Default plot when no column is selected
         ggplot() +
           geom_histogram(fill = "steelblue", color = "black") +
           labs(x = "Data", y = "Count", title = "Histogram")
@@ -187,39 +234,45 @@ app1_server <- function(id) {
     })
   })
 }
-
 # Define app2_server module
 app2_server <- function(id) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
     
+    # Reactive expression to read the uploaded CSV file
     data <- reactive({
       req(input$file)
       read.csv(input$file$datapath, header = TRUE, row.names = 1)
     })
     
+    # Reactive expression for filtered data based on variance and non-zero counts
     filteredData <- reactive({
       req(data())
       df <- data()
       
+      # Calculate variances, medians and non-zero counts for each gene (row)
       variances <- apply(df, 1, var)
       medians <- apply(df, 1, median)
       non_zero_counts <- rowSums(df > 0)
       
+      # Calculate thresholds for variance and non-zero counts
       variance_threshold <- quantile(variances, input$varianceThreshold / 100)
       non_zero_threshold <- ceiling(input$nonZeroThreshold * ncol(df) / 100)
       
+      # Filter genes based on the thresholds
       filtered_df <- df[variances >= variance_threshold & non_zero_counts >= non_zero_threshold, ]
       
       filtered_df
     })
     
+    # Render the summary table
     output$summaryTable <- renderTable({
       req(data())
       req(filteredData())
       df <- data()
       filtered_df <- filteredData()
       
+      # Calculate summaries
       total_samples <- ncol(df)
       total_genes <- nrow(df)
       passing_genes <- nrow(filtered_df)
@@ -228,6 +281,7 @@ app2_server <- function(id) {
       passing_percentage <- round((passing_genes / total_genes) * 100, 2)
       not_passing_percentage <- round((not_passing_genes / total_genes) * 100, 2)
       
+      # Create summary table
       summary_df <- data.frame(
         Category = c("Total Samples", "Total Genes", "Passing Genes", "Not Passing Genes"),
         Count = c(total_samples, total_genes, passing_genes, not_passing_genes),
@@ -237,16 +291,19 @@ app2_server <- function(id) {
       summary_df
     }, rownames = FALSE)
     
+    # Render scatter plots
     output$scatterPlots <- renderPlot({
       req(data())
       req(filteredData())
       df <- data()
       filtered_df <- filteredData()
       
+      # Calculate variances, medians, and zero counts
       variances <- apply(df, 1, var)
       medians <- apply(df, 1, median)
       zero_counts <- rowSums(df == 0)
       
+      # Create a data frame for plotting
       plot_df <- data.frame(
         Gene = rownames(df),
         Variance = variances,
@@ -255,6 +312,7 @@ app2_server <- function(id) {
         Passing = rownames(df) %in% rownames(filtered_df)
       )
       
+      # Create scatter plots
       scatter_plot1 <- ggplot(plot_df) +
         geom_point(aes(x = log10(Median), y = log10(Variance), color = Passing), alpha = 0.6) +
         scale_color_manual(values = c("lightblue", "blue")) +
@@ -262,24 +320,28 @@ app2_server <- function(id) {
         theme_minimal()
       
       scatter_plot2 <- ggplot(plot_df) +
-        geom_point(aes(x = log10(Median), y = ZeroCount, color = Passing), alpha = 0.6) +
+        geom_point(aes(x = log10(Median), y = ZeroCount,color = Passing), alpha = 0.6) +
         scale_color_manual(values = c("lightblue", "blue")) +
         labs(x = "Log10(Median Count)", y = "Number of Zeros", title = "Median Count vs Number of Zeros") +
         theme_minimal()
-      
+
+      # Combine the two scatter plots in a grid
       grid.arrange(scatter_plot1, scatter_plot2, ncol = 2)
     })
-    
+
+    # Render heatmap of the filtered data
     output$heatmap <- renderPlot({
       req(filteredData())
       filtered_df <- filteredData()
-      
+
+      # If the logTransform option is selected, apply the log2 transformation to the data
       if (input$logTransform) {
         heatmap_data <- log2(filtered_df + 1)
       } else {
         heatmap_data <- filtered_df
       }
-      
+
+      # Create the heatmap using the pheatmap function
       pheatmap(
         heatmap_data,
         cluster_rows = TRUE,
@@ -290,22 +352,27 @@ app2_server <- function(id) {
         legend = TRUE
       )
     })
-    
+
+    # Render PCA plot of the filtered data
     output$pca <- renderPlot({
       req(filteredData())
       filtered_df <- filteredData()
-      
+
+      # Perform PCA on the transposed filtered data
       pca <- prcomp(t(filtered_df), scale = TRUE)
-      
+
+      # Create a data frame from the PCA results for plotting
       pca_df <- data.frame(
         PC1 = pca$x[, 1],
         PC2 = pca$x[, 2],
         Sample = rownames(pca$x),
         Color = factor(rownames(pca$x))
       )
-      
+
+      # Calculate the proportion of variance explained by the first two principal components
       explained_variance <- round((pca$sdev^2 / sum(pca$sdev^2))[1:2] * 100, 2)
-      
+
+      # Create the PCA scatter plot
       pca_plot <- ggplot(pca_df) +
         geom_point(aes(x = PC1, y = PC2, color = Color), alpha = 0.6) +
         labs(
@@ -315,13 +382,11 @@ app2_server <- function(id) {
         ) +
         theme_minimal() +
         theme(legend.title = element_blank())
-      
+
       pca_plot
     })
   })
-}
-
-      
+}     
 
 # Define app3_server module
 # Define app3_server module
